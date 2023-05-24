4bd5139331e27b70703084c95b6dcedb2da43f2c
kd448212 <kd448212@students.mimuw.edu.pl>
2023-05-24 14:57:28 +0200

    Define interfaces

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,4 +1,16 @@
 public interface Cipher {
+    /**
+     * Encrypts a single word
+     *
+     * @param word Word to encrypt
+     * @return Encrypted word
+     */
     String encrypt(String word);
+    /**
+     * Decrypts a single word
+     *
+     * @param word Word to decrypt
+     * @return Decrypted word
+     */
     String decrypt(String word);
 }
--- src/WordLoader.java
+++ src/WordLoader.java
@@ -1,5 +1,25 @@
 public interface WordLoader {
+
+    /**
+     * Loads words in the order they appear in the file
+     *
+     * @param filename
+     * @return Array of words
+     */
     String[] loadWords(String filename);
+
+    /**
+     * Loads words in random order
+     *
+     * @param filename
+     * @return Array of words
+     */
     String[] loadWordsUnordered(String filename);
+    /**
+     * Loads words in alphabetic order
+     *
+     * @param filename
+     * @return Array of words
+     */
     String[] loadWordsAlphabetic(String filename);
 }
