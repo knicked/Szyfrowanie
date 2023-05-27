22d929c53d14a06a85716ba526e94a296c3c158c
Robert Grigoryan <rg448242@students.mimuw.edu.pl>
2023-05-27 19:52:26 +0200

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import commandlineparser.*;
+import wordaccess.FileWordLoader;
+import wordaccess.FileWordSaver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Parameters parameters = new CommandLineParser().getParameters(args);
+
+            String[] words = new FileWordLoader()
+                    .loadWordsFromFile(parameters.inputFileName());
+
+            String[] processedWords = processWords(words, parameters);
+
+            new FileWordSaver().saveWordsToFile(processedWords,
+                    parameters.outputFileName());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private static String[] processWords(String[] words, Parameters parameters) {
+        String[] processedWords = words.clone();
+
+        for (int i = 0; i < words.length; i++) {
+            switch (parameters.action()) {
+                case Encrypt ->
+                    processedWords[i] = parameters.cipher().encrypt(words[i]);
+                case Decrypt ->
+                    processedWords[i] = parameters.cipher().decrypt(words[i]);
+            }
+        }
+
+        return processedWords;
     }
 }
