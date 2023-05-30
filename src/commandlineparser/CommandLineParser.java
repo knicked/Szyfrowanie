d47f4710ceff97ec44c107d6137a23766b882b84
Robert Grigoryan <rg448242@students.mimuw.edu.pl>
2023-05-30 21:20:33 +0200

    Add CommandLineParser

--- src/commandlineparser/CommandLineParser.java
+++ src/commandlineparser/CommandLineParser.java
@@ -9,16 +9,17 @@ import org.apache.commons.cli.Options;
 
 public class CommandLineParser {
     public Parameters getParameters(String[] args) throws Exception {
-        if (args.length < 2)
-            throw new Exception("Please provide input and output file names.");
-
         Options options = getAllPossibleOptions();
 
         CommandLine cmd = new DefaultParser().parse(options, args);
 
+        if (cmd.getArgs().length != 2)
+            throw new Exception("Please provide input and output file names.");
+
         return new Parameters(
                 getAction(cmd),
                 getCipher(cmd),
+                getWordOrder(cmd),
                 args[args.length - 2],
                 args[args.length - 1]
         );
@@ -31,6 +32,8 @@ public class CommandLineParser {
         options.addOption("d", false, "decode");
         options.addOption("m", false, "use Morse code");
         options.addOption("c", true, "use Caesar code");
+        options.addOption("u", false, "get unordered words");
+        options.addOption("a", false, "get words in alphabetical order");
 
         return options;
     }
@@ -43,9 +46,10 @@ public class CommandLineParser {
 
     private Cipher getCipher(CommandLine cmd) throws Exception {
         try {
-            if (cmd.hasOption("m")) {
-                return new CezarCipher(Integer.getInteger(cmd.getOptionValue("m")));
-            } else if (cmd.hasOption("m")) {
+            if (cmd.hasOption("c")) {
+                return new CezarCipher(Integer.parseInt(cmd.getOptionValue("c")));
+            }
+            else if (cmd.hasOption("m")) {
                 return new MorsCipher();
             }
             else {
@@ -56,5 +60,11 @@ public class CommandLineParser {
             throw new Exception(cmd.getOptionValue("m") +
                     " is not a valid offset for the Caesar cipher.");
         }
+    }
+
+    private WordOrder getWordOrder(CommandLine cmd) {
+        if (cmd.hasOption("u")) return WordOrder.Unordered;
+        else if (cmd.hasOption("a")) return WordOrder.Alphabetic;
+        else return WordOrder.SameAsInputFile;
     }
 }
