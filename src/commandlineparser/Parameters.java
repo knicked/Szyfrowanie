53bc964aea183a1a495768fb0e1fe3a022eb1efc
Robert Grigoryan <rg448242@students.mimuw.edu.pl>
2023-05-30 21:19:52 +0200

    Add Parameters record class

--- src/commandlineparser/Parameters.java
+++ src/commandlineparser/Parameters.java
@@ -5,5 +5,6 @@ import cipher.Cipher;
 public record Parameters(
         Action action,
         Cipher cipher,
+        WordOrder wordOrder,
         String inputFileName,
         String outputFileName) {}
