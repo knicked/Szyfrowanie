4f5aad20e4ad6bce92db0169524226d4c5a0aa03
Robert Grigoryan <rg448242@students.mimuw.edu.pl>
2023-05-31 14:37:00 +0200

    Import cypher package

--- Tests/CezarCipherTest.java
+++ Tests/CezarCipherTest.java
@@ -1,3 +1,5 @@
+import cipher.CezarCipher;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CezarCipherTest {
--- src/cipher/MorseCipher.java
+++ src/cipher/MorseCipher.java
@@ -1,3 +1,5 @@
+package cipher;
+
 import java.util.HashMap;
 import java.util.Map;
 
